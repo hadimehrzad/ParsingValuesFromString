6f5f2b755acfb64b50ef8d780663992e32aa5e4b
hadimehrzad <61123461+hadimehrzad@users.noreply.github.com>
2020-02-27 22:37:21 +0330

    Ex22: Parsing Values From String

---
+++ src/com/HMehrzad/Learning/Main.java
@@ -0,0 +1,14 @@
+package com.HMehrzad.Learning;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        String numberAsString = "2018";
+        System.out.println("numberAsString= " + numberAsString);
+        int number = Integer.parseInt(numberAsString);
+        System.out.println("number = " + number);
+        number += 1;
+        System.out.println("number = " + number);
+    }
+}
